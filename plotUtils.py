2a04c7b69fd7ee184b50c7e85b3f26bf283715f7
Mantas <man.petrikas@gmail.com>
2018-07-04 15:44:01 +0200

    some graphing fixes

--- plotUtils.py
+++ plotUtils.py
@@ -5,13 +5,16 @@ from matplotlib.transforms import Bbox, TransformedBbox
 
 def plot(matrix):
    
-    fig,ax = plt.subplots()
+    _,ax = plt.subplots()
     ax.imshow(matrix)
 
      
-    ax.set_xticks(np.arange(len(matrix[0]))[::2]+1)
+    #ax.set_xticks(np.arange(len(matrix[0]))[::2]+1)
     ax.set_yticks(np.arange(len(matrix))[::2])    
-    ax.set_xticks(np.arange(len(matrix[0]))[::2] - 0.5, minor=True)
+    # ax.set_xticks(np.arange(len(matrix[0]))[::2] - 0.5, minor=True)
     ax.set_yticks(np.arange(len(matrix))[::2] - 0.5, minor=True)
+    im = ax.imshow(matrix)
+    cbar = ax.figure.colorbar(im,  ax=ax)
+    cbar.ax.set_ylabel("colors", rotation=-90, va="bottom")
     plt.draw()
     plt.show()
