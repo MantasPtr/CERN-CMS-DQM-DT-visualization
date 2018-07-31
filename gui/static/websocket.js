fb4f9b33ce7ff2e5fac7e2bf65d9b127764ccb99
Mantas <man.petrikas@gmail.com>
2018-07-31 15:55:00 +0200

    removed ws

--- gui/static/websocket.js
+++
@@ -1,4 +0,0 @@
-print(window.location.host)
-// var socket = new WebSocket("ws://" +  window.location.host  )
-// socket.onmessage = console.log
-// socket.send("Sending something using webSockets!");
