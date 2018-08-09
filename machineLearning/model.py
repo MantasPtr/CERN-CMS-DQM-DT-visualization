1481d1106215dee5675b3265e2467e5524db9ccf
Mantas <man.petrikas@gmail.com>
2018-08-09 11:22:36 +0200

    Fixed threading issue

--- machineLearning/model.py
+++ machineLearning/model.py
@@ -1,17 +1,24 @@
 from keras.models import load_model
 import numpy as np
 import machineLearning.transform as transform
+import tensorflow as tf
 MODELS_DIRECTORY = "./machineLearning"
 MODEL_FILE = "cnn.h5"
 
+
+cnn_model = load_model("%s/%s" % (MODELS_DIRECTORY, MODEL_FILE))
+graph = tf.get_default_graph()
+
 class Model():
 
-    def __init__(self):
-        self.cnn_model = load_model("%s/%s" % (MODELS_DIRECTORY, MODEL_FILE))
+    # def __init__(self):
+        # print(f"MODEL:{self}")
         
-    def predict(self,matrix) -> np.ndarray:
-        return self.cnn_model.predict(np.array(matrix))[:, 1]
+    def predict(self, matrix) -> np.ndarray:
+        return cnn_model.predict(np.array(matrix))[:, 1]
 
     def getScoreForMatrix(self, matrix) -> np.ndarray:
-        matrix = transform.processMatrix(matrix)
-        return self.predict(matrix)
+        global graph
+        with graph.as_default():
+            matrix = transform.processMatrix(matrix)
+            return self.predict(matrix)
