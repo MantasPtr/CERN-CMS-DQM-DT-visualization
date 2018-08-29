fixed score calculation

machineLearning/model.py
@@ -12,10 +12,18 @@ cnn_model = load_model("%s/%s" % (MODELS_DIRECTORY, MODEL_FILE))
 graph = tf.get_default_graph()
 vanilla = GradientSaliency(cnn_model)
 
-def _predict_badness(matrix) -> np.ndarray:
-    return cnn_model.predict(np.array(matrix))[:, 1]
+import time 
+def _predict_badness(matrix) -> list:
+    badLayers = list(map(_predict_layers_badness, matrix))        
+    return badLayers
 
-def get_network_score(matrix) -> np.ndarray:
+def _predict_layers_badness(layer) -> np.ndarray:
+    if len(layer) == 0:
+        return -1
+    layer_result_list = cnn_model.predict(np.array([layer]))[:, 1]
+    return layer_result_list[0].item()
+
+def get_network_score(matrix) -> list:
     global graph
     with graph.as_default():
         matrix = transform.processMatrix(matrix, MATRIX_DIM)

machineLearning/saliency.py
@@ -10,8 +10,13 @@ class GradientSaliency():
         self.compute_gradients = K.function(inputs = input_tensors, outputs = gradients)
 
     def get_mask(self, input_image):# Execute the function to compute the gradient
-        gradients = [self.compute_gradients([[input_line]])[0][0].tolist() for input_line in input_image ]   
+        gradients = list(map(self._compute_mask_for_line, input_image))
         return gradients
+
+    def _compute_mask_for_line(self, line: list) -> list:
+        if len(line) == 0:
+            return [0]
+        return self.compute_gradients([[line]])[0][0].tolist() 
         
 # https://github.com/experiencor/deep-viz-keras/blob/master/visual_backprop.py
 class VisualBackprop():
@@ -29,7 +34,6 @@ class VisualBackprop():
 
         for i in range(len(self.model.layers) - 1, -1, -1):
             if 'Conv1D' in str(type(self.model.layers[i])):  #CHANGED HERE
-                #print(np.mean(layer_outs[i], axis=2))
                 layer = np.mean(layer_outs[i], axis = 2, keepdims = True)  #CHANGED HERE
                 layer = layer - np.min(layer)
                 layer = layer / (np.max(layer) - np.min(layer) + 1e-6)

machineLearning/transform.py
@@ -9,6 +9,8 @@ def resizeMatrix(matrix, model_dim):
     return [resizeLayer(line, model_dim) for line in matrix]
 
 def resizeLayer(layer: list, model_dim) -> np.ndarray:
+    if len(layer) == 0:
+        return np.array(layer)  
     """Resizes occupancy to a given size using bilinear interpolation"""
     return resize(np.array(layer).reshape(1, len(layer)), (1, model_dim), preserve_range=True, mode='constant', anti_aliasing=False)[0]
 
@@ -16,14 +18,15 @@ def scaleMatrix(matrix):
     return list(map(scaleLayer, matrix))
 
 def scaleLayer(layer: np.ndarray) -> np.ndarray : 
+    if len(layer) == 0:
+        return layer
     layer = layer.reshape(-1, 1)
     layer = SCALER().fit(layer).transform(layer)
     return layer.reshape(1, -1)[0]
 
 def removeNegatives(matrix):
     positive =  [[x for x in line if x >= 0 ] for line in matrix]
-    notEmpty = [line for line in positive if len(line) > 0]
-    return notEmpty
+    return positive
 
 def processMatrix(matrix, model_dim):
     matrix = removeNegatives(matrix)