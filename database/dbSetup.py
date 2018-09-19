769098b53b055343cd9203b7c109f93fab772ce5
Mantas <man.petrikas@gmail.com>
2018-09-19 14:25:11 +0200

    dbSetup update

--- database/dbSetup.py
+++ database/dbSetup.py
@@ -1,25 +1,29 @@
 from config.configUtils import getConfig
 from database.mongoController import Mongo_4_DB_controller
-from database.mongoDbFactory import MongoDbFactory
+from pymongo import MongoClient, collection, operations
 
 DB_CONFIG_LOCATION='database/config/mongodb.config.ini'
-_db_controller = None
+_default_db_controller = None
+_client = MongoClient()
 
-def init_database() -> Mongo_4_DB_controller:
-    db, col = get_default_database_config()
-    collection = MongoDbFactory(db).getCollection(col)
+def get_db_controller(database: str = None, collection: str = None)-> Mongo_4_DB_controller:
+    if (database == None and  collection == None):
+        global _default_db_controller
+        _default_db_controller = _default_db_controller or _init_default_controller()
+        return _default_db_controller
+    else:
+        return Mongo_4_DB_controller(_get_collection(database, collection))
+
+def _init_default_controller() -> Mongo_4_DB_controller:
+    db, col = _get_default_database_config()
+    collection = _get_collection(db,col)
     return Mongo_4_DB_controller(collection)
 
-def get_default_database_config():
+def _get_default_database_config():
     config = getConfig(DB_CONFIG_LOCATION)
     database = config["database"]
     collection = config["collection"]
     return database, collection
 
-def get_db_controller():
-    global _db_controller
-    if not _db_controller:
-        _db_controller = init_database()
-    return _db_controller 
-
-
+def _get_collection(db_name: str, collection_name: str, client = _client):
+    return client[db_name][collection_name]
