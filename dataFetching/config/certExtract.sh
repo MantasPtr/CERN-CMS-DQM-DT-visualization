4b611c3078be58a60f10ed466f6f41406de77b55
Mantas <man.petrikas@gmail.com>
2018-09-24 13:33:40 +0200

    testing cert reading

--- dataFetching/config/certExtract.sh
+++ dataFetching/config/certExtract.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
-if [ ! "$1" != "" ]; then
-    echo "Please provide path to certificate"
-    exit 1
-fi
+# if [ ! "$1" != "" ]; then
+#     echo "Please provide path to certificate"
+#     exit 1
+# fi
 
-openssl pkcs12 -in $1 -nocerts -out key.pem
-openssl pkcs12 -in $1 -clcerts -nokeys -out cert.pem
+# openssl pkcs12 -in $1 -nocerts -out key.pem
+# openssl pkcs12 -in $1 -clcerts -nokeys -out cert.pem
+
+CERT_PATH=awk -F "=" '/cert_path / {print $2}' auth.config.ini | tr -d ' '
+CERT_KEY_PATH=awk -F "=" '/cert_key_path / {print $2}' auth.config.ini | tr -d ' '
+echo $CERT_KEY_PATH
+echo $CERT_PATH
