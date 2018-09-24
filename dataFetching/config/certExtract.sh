29ae4687d4c8da5f649b5696aa4235ea97513ba4
Mantas <man.petrikas@gmail.com>
2018-09-24 15:09:08 +0200

    added missing decoding step

--- dataFetching/config/certExtract.sh
+++ dataFetching/config/certExtract.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+set -e
 
 if [ ! "$1" != "" ]; then
     echo "Missing 1 argument - path to certificate! Exiting."
     exit 1
 fi
 
-
 CERT_PATH=$(awk -F "=" '/cert_path / {print $2}' auth.config.ini | tr -d ' ')
 CERT_KEY_PATH=$(awk -F "=" '/cert_key_path / {print $2}' auth.config.ini | tr -d ' ')
 
 openssl pkcs12 -in $1 -clcerts -nokeys -out $HOME/$CERT_PATH
-openssl pkcs12 -in $1 -nocerts -out $HOME/$CERT_KEY_PATH
+openssl pkcs12 -in $1 -nocerts -out __temp_file.userkey_encrypted.pem
+openssl rsa -in __temp_file.userkey_encrypted.pem -out $HOME/$CERT_KEY_PATH
+rm __temp_file.userkey_encrypted.pem
+chmod go-rw $HOME/$CERT_KEY_PATH
