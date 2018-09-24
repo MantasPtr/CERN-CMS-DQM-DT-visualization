cc63ff2da63aabf7a427a61a6eacc5243f22158a
Mantas Petrikas <mpetrika@mantas-image.cern.ch>
2018-09-24 14:13:08 +0200

    Fixed reading config

--- dataFetching/config/certExtract.sh
+++ dataFetching/config/certExtract.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! "$1" != "" ]; then
-    echo "Please provide path to certificate"
+    echo "Missing 1 argument - path to certificate! Exiting."
     exit 1
 fi
 
@@ -9,5 +9,5 @@ fi
 CERT_PATH=$(awk -F "=" '/cert_path / {print $2}' auth.config.ini | tr -d ' ')
 CERT_KEY_PATH=$(awk -F "=" '/cert_key_path / {print $2}' auth.config.ini | tr -d ' ')
 
-openssl pkcs12 -in $1 -clcerts -nokeys -out $CERT_PATH
-openssl pkcs12 -in $1 -nocerts -out $CERT_KEY_PATH
+openssl pkcs12 -in $1 -clcerts -nokeys -out $HOME/$CERT_PATH
+openssl pkcs12 -in $1 -nocerts -out $HOME/$CERT_KEY_PATH
